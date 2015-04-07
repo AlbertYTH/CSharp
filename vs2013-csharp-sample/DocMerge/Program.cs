ae8db2cd133f8a93f2dcdab8a5e708b60c97a0b8
AlbertYTH <10015xx@gmail.com>
2015-04-07 21:29:55 +0800

    完成讀取data檔功能

---
+++ vs2013-csharp-sample/DocMerge/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int counter = 0;
+            string line;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            //string[] words = text.Split(delimiterChars);
+            System.IO.StreamReader file = new System.IO.StreamReader("C:/Users/Albert/Desktop/敏捷軟體/Git/CSharp/vs2013-csharp-sample/DocMerge/data.txt");
+            while((line = file.ReadLine()) != null)
+            {
+               Console.WriteLine (line);
+               counter++;
+            }
+
+            file.Close();
+          
+            Console.ReadLine();
+            
+            
+            /*System.Console.WriteLine("{0} words in text:", words.Length);
+
+            foreach (string s in words)
+            {
+                System.Console.WriteLine(s);
+            }*/
+
+        }
+    }
+    
+}
+
