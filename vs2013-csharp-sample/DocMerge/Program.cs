3c33ca35dfe683ae8c2cb1cf7c9e6419aef4839b
AlbertYTH <10015xx@gmail.com>
2015-04-08 15:44:54 +0800

    完成根據template以及data資料，產生result檔

--- vs2013-csharp-sample/DocMerge/Program.cs
+++ vs2013-csharp-sample/DocMerge/Program.cs
@@ -13,35 +13,66 @@ namespace DocMerge
 
 
             int counter = 0;
-            string line;
-            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
-            //string[] words = text.Split(delimiterChars);
-            System.IO.StreamReader file = new System.IO.StreamReader("C:/Users/Albert/Desktop/敏捷軟體/Git/CSharp/vs2013-csharp-sample/DocMerge/data.txt");
-            while((line = file.ReadLine()) != null)
+            string data_txt = "aa", template_txt = "cc";
+            string[] data_list = { };
+            List<string> template_txt_copy = new List<string>();
+            //List<string> result_txt = new List<string>();
+            char[] delimiterChars = { '\t' };//' ', ',', '.', ':', 
+            data_list = new string[] { template_txt };
+            System.IO.StreamReader template_file = new System.IO.StreamReader("C:/Users/Albert/Desktop/敏捷軟體/Git/CSharp/vs2013-csharp-sample/DocMerge/template.txt");
+            counter = 0;
+            while ((template_txt = template_file.ReadLine()) != null)
             {
-               Console.WriteLine (line);
-               counter++;
-            }
-
-            file.Close();
-            System.IO.StreamReader file2 = new System.IO.StreamReader("C:/Users/Albert/Desktop/敏捷軟體/Git/CSharp/vs2013-csharp-sample/DocMerge/template.txt");
-            while ((line = file2.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
+                //Console.WriteLine(template_txt);
+                template_txt_copy.Add(template_txt);
                 counter++;
             }
+            template_file.Close();
 
-            file.Close();
-            Console.ReadLine();
-            
-            
-            /*System.Console.WriteLine("{0} words in text:", words.Length);
+            /*foreach (string s in result_txt)
+                Console.WriteLine(s);*/
 
-            foreach (string s in words)
+
+            System.IO.StreamReader data_file = new System.IO.StreamReader("C:/Users/Albert/Desktop/敏捷軟體/Git/CSharp/vs2013-csharp-sample/DocMerge/data.txt");
+            System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:/Users/Albert/Desktop/敏捷軟體/Git/CSharp/vs2013-csharp-sample/DocMerge/result.txt");
+
+            counter = 0;
+            data_txt = data_file.ReadLine();
+            while ((data_txt = data_file.ReadLine()) != null)
             {
-                System.Console.WriteLine(s);
-            }*/
+                //Console.WriteLine(data_txt);
+                data_list = data_txt.Split(delimiterChars);
+                string[] result_txt = new string[4];
+                for (int i = 0; i < 4; i++)
+                    result_txt[i] = template_txt_copy[i];
+                for (int i = 0; i < 4; i++)
+                { 
+                    result_txt[i] = result_txt[i].Replace("${中文姓名}", data_list[0]);
+                    result_txt[i] = result_txt[i].Replace("${身份證字號}", data_list[1]);
+                    result_txt[i] = result_txt[i].Replace("${年數}", data_list[2]);
+                }
+                //foreach (string s in result_txt)
+                //    Console.WriteLine(s);
+                for (int i = 0; i < 4; i++)
+                {
+                    file.WriteLine(result_txt[i]);
+                    Console.WriteLine(result_txt[i]);
+                }
+                counter++;
+            }
+            data_file.Close();
+                file.Close();
 
+
+
+            //foreach (string s in data_list)
+            //    Console.WriteLine(s);
+            //string  aa ;
+            //    aa= "aaaaaaaajifahgoah" ;
+            //aa=aa.Replace("aa", "i");
+            //Console.WriteLine(aa);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
     
